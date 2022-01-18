ab095a441368caf1c8b8bae6a52c681a03183c85
Jax <kingsoprano718@gmail.com>
2022-01-18 18:38:38 -0500

    First Commit

---
+++ Assets/MapMinimap/Scripts/MapSettingsData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MapMinimap
+{
+
+    [CreateAssetMenu(fileName = "MapSettings", menuName = "Data/MapSettings", order = 10)]
+    public class MapSettingsData : ScriptableObject
+    {
+        [Header("Zoom")]
+        public float zoom_max = 4f;
+
+        [Header("Icons")]
+        public int icon_size = 32;
+        public int icon_refresh_count = 100;
+
+        [Header("Fog")]
+        public bool fog = true;
+        public float fog_reveal_radius = 10f;
+        public int fog_precision = 100;
+        public float fog_update_rate = 0.5f;
+    }
+
+}
